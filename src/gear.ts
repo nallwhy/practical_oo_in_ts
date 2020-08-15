fe3f352e7fa29240a09cc21119130d07a49ab384
Json <nallwhy@gmail.com>
2020-08-15 20:21:29 +0900

    [2.2.1] Add Gear.getGearInches()

--- src/gear.ts
+++ src/gear.ts
@@ -1,14 +1,26 @@
 export class Gear {
     private chainring: number
     private cog: number
+    // rim diameter
+    private rim: number
+    // tire height
+    private tire: number
 
-    constructor(chainring: number, cog: number) {
+    constructor(chainring: number, cog: number, rim: number, tire: number) {
         this.chainring = chainring
         this.cog = cog
+        this.rim = rim
+        this.tire = tire
     }
 
     // ratio = chainring / cog
     public getRatio(): number {
         return this.chainring / this.cog
+    }
+
+    // gear inches = tire diameter * ratio
+    // tier diameter = rim diameter + tire height * 2
+    public getGearInches(): number {
+        return (this.rim + (this.tire * 2)) * this.getRatio()
     }
 }
--- test/gear.test.ts
+++ test/gear.test.ts
@@ -2,10 +2,20 @@ import { Gear } from "../src/gear"
 
 describe("getRatio()", () => {
     test("returns chainring / cog", () => {
-        const gear0: Gear = new Gear(52, 11)
-        expect(gear0.getRatio()).toBeCloseTo(52 / 11)
+        const gear0: Gear = new Gear(52, 11, 0, 0)
+        expect(gear0.getRatio()).toBeCloseTo(4.727)
 
-        const gear1: Gear = new Gear(30, 27)
-        expect(gear1.getRatio()).toBeCloseTo(30 / 27)
+        const gear1: Gear = new Gear(30, 27, 0, 0)
+        expect(gear1.getRatio()).toBeCloseTo(1.111)
+    })
+})
+
+describe("getGearInches()", () => {
+    test("returns tire diameter * ratio", () => {
+        const gear0: Gear = new Gear(52, 11, 26, 1.5)
+        expect(gear0.getGearInches()).toBeCloseTo(137.09)
+
+        const gear1: Gear = new Gear(52, 11, 24, 1.25)
+        expect(gear1.getGearInches()).toBeCloseTo(125.272)
     })
 })
