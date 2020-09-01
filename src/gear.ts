88313503d9e024a92bb3ca34d72a44d65a6b8018
Json <nallwhy@gmail.com>
2020-09-01 21:19:46 +0900

    [2.3.1] Hide instance variables

--- src/gear.ts
+++ src/gear.ts
@@ -1,16 +1,28 @@
 export class Gear {
-    private chainring: number
-    private cog: number
+    private _chainring: number
+    private get chainring(): number {
+        return this._chainring
+    }
+    private _cog: number
+    private get cog(): number {
+        return this._cog
+    }
     // rim diameter
-    private rim: number
+    private _rim: number
+    private get rim(): number {
+        return this._rim
+    }
     // tire height
-    private tire: number
+    private _tire: number
+    private get tire(): number {
+        return this._tire
+    }
 
     constructor(chainring: number, cog: number, rim: number, tire: number) {
-        this.chainring = chainring
-        this.cog = cog
-        this.rim = rim
-        this.tire = tire
+        this._chainring = chainring
+        this._cog = cog
+        this._rim = rim
+        this._tire = tire
     }
 
     // ratio = chainring / cog
