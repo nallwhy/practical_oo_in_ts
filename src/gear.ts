cb80c8eac34e9e6485fad2ea10d5b8acfa8ea4fe
Json <nallwhy@gmail.com>
2020-09-03 03:58:47 +0900

    Revert "[3.2.2] Isolate instance creation - 1"
    This reverts commit ce37f5200faff98334bf1249d0ec9c182b6ee2d1.

--- src/gear.ts
+++ src/gear.ts
@@ -1,5 +1,3 @@
-import { Wheel } from "./wheel"
-
 export class Gear {
     private _chainring: number
     private get chainring(): number {
@@ -9,22 +7,28 @@ export class Gear {
     private get cog(): number {
         return this._cog
     }
-    private _wheel: Wheel
-    private get wheel(): Wheel {
+    private _wheel: IWheel
+    private get wheel(): IWheel {
         return this._wheel
     }
 
-    constructor(chainring: number, cog: number, rim: number, tire: number) {
+    constructor(chainring: number, cog: number, wheel: IWheel) {
         this._chainring = chainring
         this._cog = cog
-        this._wheel = new Wheel(rim, tire)
+        this._wheel = wheel
     }
 
+    // ratio = chainring / cog
     public get ratio(): number {
         return this.chainring / this.cog
     }
 
+    // gear inches = tire diameter * ratio
     public get gearInches(): number {
         return this.ratio * this.wheel.diameter
     }
+}
+
+interface IWheel {
+    readonly diameter: number
 }
